b3c93f274a0b2f923b0f88a081663286ed204574
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-02-23 16:27:20 +0000

    ...

---
+++ iblrigcore/sync_status.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# @File: iblrigcore/sync_status.py
+# @Author: Niccolo' Bonacchi (@nbonacchi)
+# @Date: Wednesday, February 23rd 2022, 4:21:36 pm
---
+++ iblrigcore/videopc/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# @File: videopc/__init__.py
+# @Author: Niccolo' Bonacchi (@nbonacchi)
+# @Date: Wednesday, February 23rd 2022, 4:20:19 pm

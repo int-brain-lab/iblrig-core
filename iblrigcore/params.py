d6bb735a30a0cba96e07cca1fd55c96561f1d083
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-03-08 15:39:10 +0000

    Params metaclass not working! Maybe inheritance is enough?

--- iblrigcore/params.py
+++ iblrigcore/params.py
@@ -2,6 +2,7 @@
 # @File: iblrigcore/params.py
 # @Author: Niccolo' Bonacchi (@nbonacchi)
 # @Date: Monday, February 28th 2022, 5:11:39 pm
+from fileinput import filename
 import json
 import logging
 import shutil
@@ -15,49 +16,108 @@ log = logging.getLogger("iblrig")
 PARAMS_FILE_PATH = Path().home().joinpath(".iblrigcore_params.json")
 
 
-class ParamFile:
+class BaseParamFile(type):
     """Simple Parameter file interaction class
 
     Returns:
         _type_: _description_
     """
-    default_params = {
+
+    default_template = {
         "MODALITY": None,
         "DATA_FOLDER_LOCAL": None,  # str
         "DATA_FOLDER_REMOTE": None,  # str
     }
-    default_filename = ".iblrigcore_params.json"
+    default_filename = f".iblrigcore_params.json"
     default_folderpath = Path().home().joinpath(".iblrigcore")
     default_filepath = default_folderpath.joinpath(default_filename)
 
     filename = None
     foderpath = None
     filepath = None
+    template = None
+
+    def __new__(
+        cls,
+        filename: str = None,
+        folderpath: Path = None,
+        filepath: Path = None,
+        template: dict = None,
+    ):
+        # obj = super().__new__(cls)
+        cls.set_file(filename=filename, folderpath=folderpath, filepath=filepath)
+        cls.set_template(template=template)
+        return cls  # if cls isn't returned the class is not initialized
 
     @classmethod
-    def set_filepath(cls, path: Path = None) -> None:
-        if path is None:
-            path = cls.default_filepath
-        path = Path(path)
-        cls.filename = path.name
-        cls.folderpath = path.parent
-        cls.filepath = cls.folderpath.joinpath(cls.filename)
+    def set_file(
+        cls, filename: str = None, folderpath: Path = None, filepath: Path = None
+    ) -> None:
+        """Initializes the ParamFile file args using either a name/folder or a path.
+        Set the filepath for the parameter file. determines the folderpath and filename.
+        Defaults are used if no input is given.
+
+
+        Args:
+            filename (str, optional): Name of param file. Defaults to None.
+            folderpath (Path, optional): folder to save it. Defaults to None.
+            filepath (Path, optional): fullpath of paramfile. Defaults to None.
+
+        Raises:
+            ValueError: If both filename/folderpath AND filepath are set.
+        """
+        if filepath and (filename or folderpath):
+            raise ValueError(
+                "You can specify either filepath or filename/folderpath."
+            )
+        if filepath:
+            filepath = Path(filepath)
+            cls.filename = filepath.name
+            cls.folderpath = filepath.parent
+            cls.filepath = cls.folderpath.joinpath(cls.filename)
+        else:
+            cls.filename = filename or cls.default_filename
+            cls.folderpath = folderpath or cls.default_folderpath
+            cls.filepath = Path(cls.folderpath).joinpath(cls.filename)
+
+    @classmethod
+    def set_template(cls, template: dict = None) -> None:
+        """Initializes the ParamFile arguments (including folderpath, filename and filepath)
+        using a given template that will be added to the default_template dictionary.
+        Set the template for the parameter file.
+        Defaults to the default_template dictionary. Will update it with a templete provided.
+
+        Args:
+            template (dict, optional): A template used to generate a new parameter file.
+                                        Defaults to None.
+        """
+        if template is None:
+            template = cls.default_template
+        else:
+            template = {**cls.default_template, **template}
+        cls.template = template
 
     @classmethod
     def write(cls, pars: dict) -> None:
+        """Write a full parameter dictionary to the file
+
+        Args:
+            pars (dict): dict has to contain all keys from template
+        """
         # Check necessary keys are in the parmas you want to write
-        assert all([x in pars for x in cls.default_params]), "Invalid params dict"
+        assert all([x in pars for x in cls.template]), "Invalid params dict"
         # Create a backup
-        ParamFile.backup()
+        cls.backup()
         # Ensure folder exists
         cls.folderpath.mkdir(exist_ok=True)
         # Write the new params truncating old file
         with open(cls.filepath, "w") as f:
-            json.dump(pars, f)
+            json.dump(pars, f, indent=2)
+        return pars
 
     @classmethod
     def read(cls, key: str = None) -> Union[dict, Any]:
-        """Read Param file, will create a default parameter file if not found.
+        """Read Parameter file. will return the template if no file is found
 
         Args:
             key (str, optional): Return only a specific key. Defaults to None.
@@ -67,7 +127,7 @@ class ParamFile:
         """
         if not cls.filepath.exists():
             log.debug("Not found: .iblrigcore_params.json creating...")
-            cls.write(cls.default_params)
+            return cls.template
 
         with open(cls.filepath, "r") as f:
             pars = json.load(f)
@@ -84,68 +144,69 @@ class ParamFile:
         Args:
             new_pars (dict): Partial dict to update the existing one
         """
-        log.debug(f"Updating .iblrigcore_params.json with {new_pars}")
+        log.debug(f"Updating {cls.filename} with {new_pars}")
         # Possible checks for unupdatable params?
-        # old_pars = ParamFile.read()
-        # old_pars.update(new_pars)
-        cls.write(cls.read().update(new_pars))
-        pass
+        old_pars = cls.read()
+        old_pars.update(new_pars)
+        cls.write(old_pars)
 
     @classmethod
     def backup(cls) -> None:
-        """Backup Param file, only one version back supported
-        """
+        """Backup Param file, only one version back supported"""
         if cls.filepath.exists():
-            shutil.move(
-                cls.filepath, cls.filepath.parent.joinpath(".iblrigcore_params.json.bak")
-            )
+            log.debug(f"Backing up {cls.filepath}...")
+            shutil.move(cls.filepath, cls.filepath.parent.joinpath(cls.filename + ".bak"))
         else:
             log.debug("ParamFile not found, nothing to backup")
 
     @classmethod
     def delete(cls) -> None:
-        """Delete Param file after backing up the old one
-        """
+        """Delete Param file after backing up the old one"""
         cls.backup()
-        cls.filepath.unlink()
+        if cls.filepath.exists():
+            log.debug(f"Deleting {cls.filepath}...")
+            cls.filepath.unlink()
+        else:
+            log.debug("ParamFile not found, nothing to delete")
 
-ParamFile.set_filepath()
-
-
-class VideoParamFile(ParamFile):
-    """Behavior Parameter file interaction class"""
     @classmethod
-    def create_videopc_params(cls):
-        # Default name and location OK
-        cls.set_filepath()
-        default_params = cls.default_params
-        vidopc_params = {
-            "BODY_CAM_IDX": None,
-            "LEFT_CAM_IDX": None,
-            "RIGHT_CAM_IDX": None,
-        }
-        # Populate with values (ask user if not found) TODO: add populate method!!
-        # Update default params with vidopc_params
-        cls.write(cls.default_params)
-        cls.update
-        print("create")
+    def populate(cls):
+        """Populate the params dict (user input required)"""
+        pars = cls.read()
+
+        for k in pars:
+            resp = input(f"{k} [{pars[k]}]: ")
+            pars[k] = pars[k] if not resp else resp
+
+        cls.update(pars)
 
 
-def create_default_local_params() -> None:
-    ParamFile.write(ParamFile.default_params)
-    return
+class VideoParamFile(BaseParamFile):
+    __metaclass__ = BaseParamFile
+    pass
 
 
-def create_local_params(params: dict) -> None:
-    if ParamFile.path.exists():
-        raise Exception("Params file already exists")
-    else:
+class EphysParamFile(BaseParamFile):
+    __metaclass__ = BaseParamFile
+    def __init__(self, *args, **kwargs) -> None:
+        self.some_var = "WTF?!?!?!"
 
-        ParamFile.write(
-            {
-                "MODALITY": modality,
-                "LOCAL_DATA_FOLDER": local_data_folder,
-                "REMOTE_DATA_FOLDER": remote_data_folder,
-            }
-        )
-    return
+    def method():
+        print("aaaa")
+
+
+video_fname = ".videopc_params.json"
+video_params = {
+    "BODY_CAM_IDX": int,
+    "LEFT_CAM_IDX": int,
+    "RIGHT_CAM_IDX": int,
+}
+ephys_fname = ".ephyspc_params.json"
+ephys_params = {
+    "PROBE_TYPE_00": int,
+    "PROBE_TYPE_01": int,
+}
+
+BaseParamFile()
+VideoParamFile(filename=video_fname, template=video_params)
+bla = EphysParamFile(filename=ephys_fname, template=ephys_params)

2ab6c684ceaaa15feec03e6e30597e57cc185bce
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-03-30 18:18:07 +0100

    Params modules for different modalities

---
+++ iblrigcore/ephyspc/params.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# @File: videopc/params.py
+# @Author: Niccolo' Bonacchi (@nbonacchi)
+# @Date: Wednesday, March 30th 2022, 12:10:48 pm
+from iblrigcore.params import ParamFile
+
+
+class EphysParamFile(ParamFile):
+    def __init__(self, *args, **kwargs) -> None:
+        self.filename = ".ephyspc_params.json"
+        self.template = {
+            "PROBE_TYPE_00": int,
+            "PROBE_TYPE_01": int,
+        }
+        self._init_class(filename=self.filename, template=self.template)
+        # super(EphysParamFile, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+
+
+EphysParamFile()
+
+
+if __name__ == "__main__":
+    EphysParamFile.create()
+    print(0)
---
+++ iblrigcore/videopc/params.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# @File: videopc/params.py
+# @Author: Niccolo' Bonacchi (@nbonacchi)
+# @Date: Wednesday, March 30th 2022, 12:10:48 pm
+from iblrigcore.params import ParamFile
+
+
+class VideoParamFile(ParamFile):
+    def __init__(self, *args, **kwargs):
+        self.video_fname = ".videopc_params.json"
+        self.video_template = {
+            "BODY_CAM_IDX": int,
+            "LEFT_CAM_IDX": int,
+            "RIGHT_CAM_IDX": int,
+        }
+        self._init_class(filename=self.video_fname, template=self.video_template)
+        # super(VideoParamFile, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+
+VideoParamFile()
+
+
+if __name__ == "__main__":
+    VideoParamFile.create()
+    print(0)
